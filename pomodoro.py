168df03fdb7bb0f9029fed8e0f18aad4ac4c4baa
Leonardo Henao <leonardofhenao.085@gmail.com>
2024-02-07 12:03:55 -0500

    Feat: Se creo GUI con tkinter

--- pomodoro.py
+++ pomodoro.py
@@ -1,150 +1,241 @@
+from datetime import datetime, timedelta
 import time
-import vlc
-from colorama import just_fix_windows_console, init, Fore
-from colorama.ansi import clear_screen
 from windows_toasts import Toast, WindowsToaster, ToastDuration
-
-just_fix_windows_console()
-init(autoreset=True, convert=True)
+import tkinter as tk
 
 # Change to False for real values
-test = False
+test = True
+
+_pattern_format_time = "%I:%M:%S %p"
 
 # Default times
-_max_section_time = 20 * 60 if not test else 10
-_max_break_time = 5 * 60 if not test else 4
-_max_sections_to_long_break = 4
-_max_long_break_time = 15 * 60 if not test else 7
+_max_section_pomo_time = 20 if not test else 1
+_max_short_break_time = 5 if not test else 1
+_max_long_break_time = 15 if not test else 2
 
 # Current sections
-section_number = 1
-
-# Messages
-message_finish_pomodoro = "¿Desear finalizar Pomodoro? [S] o [N]: "
-message_error = "Opción invalida 👾"
+pomo_count = 0
+break_count = 0
+long_break_count = 0
 
 
-def print_seconds(arg):
-    minutes = int(arg / 60)
-    minutes = str(minutes) if minutes > 9 else str(f"0{minutes}")
-    seconds = arg % 60
-    seconds = str(seconds) if seconds > 9 else str(f"0{seconds}")
-
-    print_formatted = f"{minutes}:{str(seconds)}"
-    print(Fore.CYAN + print_formatted, end='\r')
-
-
-def print_message_app(message):
-    print(Fore.YELLOW + message)
-
-
-def print_error(message):
-    print(Fore.RED + message)
-
-
-def send_notification(
-    message: str, is_long: bool = True, title: str = "Pomodoro leonardohenao.com"
-):
+def send_notification(message: str, is_long: bool = True, title: str = "Pomodoro"):
     toaster = WindowsToaster(title)
     new_toast = Toast([message])
     new_toast.duration = ToastDuration.Long if is_long else ToastDuration.Default
     toaster.show_toast(new_toast)
 
 
-def start_player(path_sound):
-    player_st = vlc.MediaPlayer(path_sound)
-    player_st.play()
+def insert_frame(master_, height_, row_, column_=1):
+    """
+    Inserta espacio en la ventana
+    """
+    frame_1 = tk.Frame(master=master_, width=6, height=height_, bg="#212121")
+    frame_1.grid(column=column_, row=row_, columnspan=6)
 
 
-def show_message_finish_pomodoro():
+def get_time():
+    """
+    Generador contador de tiempo
+    """
     while True:
-        options_close = input(message_finish_pomodoro)
-        if options_close.lower() == 's':
-            exit()
-        else:
-            break
-
-
-def control_pomodoro():
-    while True:
-        options = input("¿Deseas continuar con el Pomodoro? [S] o [N]: ")
-        if options.lower() == 's':
-            start_pomodoro()
-            break
-        elif options.lower() == 'n':
-            show_message_finish_pomodoro()
-        else:
-            print_error(message_error)
-            continue
-
-
-def start_break(long: bool = False):
-    count = 0
-    time_to_break = _max_break_time if not long else _max_long_break_time
-    start_player("/media/break_time.mp3")
-    while True:
+        tm = datetime.now()
         time.sleep(1)
-        count += 1
-        print_seconds(time_to_break - count)
-        if count == time_to_break:
-            send_notification("¡Tiempo de descanso finalizado!")
-            control_pomodoro()
+        yield tm.strftime(_pattern_format_time)
+
+
+def get_finish_time(min: int):
+    """
+    Obtiene el tiempo en que debe finalizar el pomodoro o la pausa
+    ...
+
+    Parameters
+    ---
+    min : int -> Minutos a sumar al tiempo actual
+    """
+    tm = datetime.now()
+    tm_delta = timedelta(minutes=min)
+    tm_finish = tm_delta + tm
+    return datetime.strftime(tm_finish, "%I:%M:%S %p")
+
+
+def UpdateWindowDecor(func):
+    def wrapper(label: tk.Label, time: str = None):
+        func(label, time) if time else func(label)
+        window.update()
+
+    return wrapper
+
+
+def finalizar_pomo_tkinder():
+    window.destroy()
+
+
+@UpdateWindowDecor
+def update_pomo_count(label: tk.Label):
+    global pomo_count
+    pomo_count += 1
+    label["text"] = f"Pomodoros: \n{pomo_count}"
+
+
+@UpdateWindowDecor
+def update_break_count(label: tk.Label):
+    global break_count
+    break_count += 1
+    label["text"] = f"Descansos cortos: \n{break_count}"
+
+
+@UpdateWindowDecor
+def update_long_break_count(label: tk.Label):
+    global long_break_count
+    long_break_count += 1
+    label["text"] = f"Descansos largos: \n{long_break_count}"
+
+
+@UpdateWindowDecor
+def update_label_finish(label: tk.Label, time: str):
+    label["text"] = f"Finaliza: {time}"
+
+
+@UpdateWindowDecor
+def update_label_time_count(label: tk.Label, time: str):
+    label["text"] = time
+
+
+def start_break():
+    next_break = _max_short_break_time if pomo_count % 4 != 0 else _max_long_break_time
+
+    time_format = get_finish_time(next_break)
+    update_label_finish(label_finish, time_format)
+
+    button_end.grid(**_button_center)
+    button_start_break.grid_remove()
+
+    for x in get_time():
+        try:
+            update_label_time_count(label_count_clock, x)
+            if x == time_format:
+                button_end.grid_remove()
+                button_start.grid(**_button_center)
+
+                if next_break == _max_short_break_time:
+                    update_break_count(label_count_break)
+                else:
+                    update_long_break_count(label_count_long_break)
+
+                send_notification("¡Descanso terminado!")
+                break
+        except:
             break
 
 
-def control_breaks():
-    global _max_sections_to_long_break, section_number
+def start_pomo():
+    global label_finish
 
-    message_start_break = "¿Deseas iniciar ya el tiempo de descanso? [S] o [N]: "
+    button_start.grid_remove()
+    button_end.grid(**_button_center)
 
-    if section_number == _max_sections_to_long_break:
-        print("Descanso largo muy merecido 🐻")
-        while True:
-            options = input(message_start_break)
-            if options.lower() == 's':
-                section_number = 1
-                start_break(long=True)
+    time_format = get_finish_time(_max_section_pomo_time)
+    update_label_finish(label_finish, time_format)
+
+    for x in get_time():
+        try:
+            update_label_time_count(label_count_clock, x)
+            if x == time_format:
+                button_end.grid_remove()
+                button_start_break.grid(**_button_center)
+                update_pomo_count(label_count_pomos)
+                send_notification("¡Pomodoro terminado!")
                 break
-            elif options.lower() == 'n':
-                show_message_finish_pomodoro()
-            else:
-                print_error(message_error)
-                continue
-    elif section_number < _max_sections_to_long_break:
-        print(f"Descanso de {int(_max_break_time / 60)}.")
-        while True:
-            options = input(message_start_break)
-            if options.lower() == 's':
-                start_break()
-                break
-            elif options.lower() == 'n':
-                show_message_finish_pomodoro()
-            else:
-                print_error(message_error)
-                continue
+        except Exception as err:
+            print(f"Error -> {err}")
+            break
 
 
-def start_pomodoro():
-    global section_number
-    count = 0
-    start_player("/media/pomodoro_start.mp3")
-    try:
-        while True:
-            time.sleep(1)
-            count += 1
-            if count == _max_section_time:
-                send_notification("¡Tiempo finalizado!")
-                start_player("/media/alert_pomodoro.mp3")
-                section_number += 1
-                control_breaks()
-                break
-            else:
-                print_seconds(_max_section_time - count)
-    except KeyboardInterrupt:
-        print_message_app("Saliendo de Pomodoro, hasta la proxima. 🧙 ")
-        exit()
+# GUI
+
+_styles = {"color_font": "ivory2", "color_background": "#212121"}
+_button_center = {"column": 3, "row": 5}
+_width_buttons = {"width": 30}
+_text_start_clock = "00:00"
+_font_poppins = lambda size=12, bold=True: ("Poppins", f"{size}", "bold" if bold else "normal")
+_label_style = {"fg": _styles["color_font"], "bg": _styles["color_background"]}
+
+# Tkinter config
+window = tk.Tk()
+window.title("Pomodoro! by Leonardo Henao")
+window.configure(bg=_styles["color_background"])
+
+# Title
+label_head = tk.Label(
+    window,
+    text="Bienvenido, \nNos alegra saber que deseas ser mas productivo.",
+    padx=4,
+    pady=2,
+    font=_font_poppins(),
+    **_label_style,
+)
+label_head.grid(column=1, row=1, columnspan=6)
+
+# Count
+label_count_clock = tk.Label(window, text=_text_start_clock, font=("Arial", "20", "roman"), **_label_style)
+label_count_clock.grid(column=1, row=2, columnspan=6, pady=2)
+
+# Finish
+label_finish = tk.Label(
+    window, text=_text_start_clock, font=("Arial", "15", "roman"), fg="red", bg=_styles["color_background"]
+)
+label_finish.grid(column=1, row=3, columnspan=6)
+
+# Space
+insert_frame(window, 50, 4)
+
+# Button
+button_start = tk.Button(
+    window,
+    text="Iniciar pomodoro",
+    command=start_pomo,
+    bg="cyan",
+    fg="black",
+    font=("poppins", "12", "bold"),
+    **_width_buttons,
+)
+button_start.grid(**_button_center)
+
+button_end = tk.Button(
+    window,
+    text="Finalizar",
+    command=finalizar_pomo_tkinder,
+    bg="red",
+    fg="white",
+    font=("poppins", "12", "bold"),
+    **_width_buttons,
+)
 
 
-print(clear_screen())
-print_message_app("Bienvenido, iniciando el Pomodoro!")
-start_pomodoro()
+button_start_break = tk.Button(
+    window,
+    text="Iniciar pausa",
+    command=start_break,
+    bg="gray",
+    fg="black",
+    font=("poppins", "12", "bold"),
+    **_width_buttons,
+)
+
+# Space
+insert_frame(window, 50, 7)
+
+# label count pomo
+label_count_pomos = tk.Label(window, text="Pomodoros: \n0", **_label_style, font=_font_poppins(bold=False))
+label_count_pomos.grid(column=2, row=8)
+
+# label count break
+label_count_break = tk.Label(window, text="Descansos cortos: \n0", **_label_style, font=_font_poppins(bold=False))
+label_count_break.grid(column=3, row=8)
+
+# label count long break
+label_count_long_break = tk.Label(window, text="Descansos largos: \n0", **_label_style, font=_font_poppins(bold=False))
+label_count_long_break.grid(column=5, row=8)
+
+window.mainloop()
